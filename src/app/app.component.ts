b6636060655b2a93ea806a35ec61d86660084804
BigSnow1 <2539820590@qq.com>
2018-03-07 11:45:34 +0800

    add one _ then ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -41,7 +41,7 @@ updateHero (hero: Hero): Observable<any> {
 /** POST: add a new hero to the server */
 addHero (hero: Hero): Observable<Hero> {
   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-    tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+    tap((_hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
     catchError(this.handleError<Hero>('addHero'))
   );
 }
